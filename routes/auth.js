6e1ce20613213ea69ddd98c2f1c4d3ee96a5e260
Tabebee <tabibone@gmail.com>
2017-11-30 14:19:45 -0500

    finished login local

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,29 @@
 var authController = require("../controllers/authcontroller.js");
 
-module.exports = function (app) {
+module.exports = function (app, passport) {
     app.get("/signup", authController.signup);
     app.get("/signin", authController.signin);
+    //  where redirect when signup success???? Also change below if different
+    app.post("/signup", passport.authenticate("local-signup", {
+        successRedirect: "/events",
+        failureRedirect: "/signup"
+        }
+    ));
+
+    function isLoggedIn(req, res, next) {
+        if (req.isAuthenticated())
+            return next();
+
+        res.redirect('/signin');
+    }
+
+    app.get("/events", isLoggedIn(), authController.guest());
+    app.get("/logout", authController.logout());
+
+    app.post("/signin", passport.authenticate("local-signin", {
+        successRedirect: "/events",
+        failureRedirect: "/signin"
+        }
+    ));
 };
 
