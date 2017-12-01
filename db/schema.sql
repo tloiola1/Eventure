d0475da17e09c1bda3596f440d777bf12367b7ba
Tarciso <tarcisoloiola@hotmail.com>
2017-11-30 19:30:07 -0500

    commiting to heroku

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,4 @@
 -- Drops the todolist if it exists currently --
-DROP DATABASE IF EXISTS socail_db;
+DROP DATABASE IF EXISTS b59m1hc62h6xpsy0;
 -- Creates the "todolist" database --
-CREATE DATABASE social_db;
+CREATE DATABASE b59m1hc62h6xpsy0;
