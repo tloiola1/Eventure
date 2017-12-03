3e790146f1da25329bb3ff9bf41494058d007546
Tarciso <tarcisoloiola@hotmail.com>
2017-12-02 21:36:46 -0500

    add retake pic button

--- public/assets/js/user.js
+++ public/assets/js/user.js
@@ -25,29 +25,53 @@ $(function() {
     });
   });
 
-$("#camera").on("click", function(){
-  $("#canvas").hide();
-var canvas = document.getElementById('canvas');
-var context = canvas.getContext('2d');
-var localStream;
-  $('#video').attr("autoplay", "true");
+  $("#camera").on("click", function(){
+     $("#canvas").hide();
+      var canvas = document.getElementById('canvas');
+      var context = canvas.getContext('2d');
+      var localStream;
+      $('#video').attr("autoplay", "true");
 
-if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-        video.src = window.URL.createObjectURL(stream);
-        localStream = stream;
+      if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+          navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+              video.src = window.URL.createObjectURL(stream);
+              localStream = stream;
+          });
+      }
+
+      document.getElementById("snap").addEventListener("click", function() {
+        
+        $("#canvas").show();
+
+        context.drawImage(video, 0, 0, 380, 280);
+        $("#video").hide();
+        localStream.getVideoTracks()[0].stop();
+      });
+
+      document.getElementById("retake").addEventListener("click", function() {
+        $("#video").show();
+        $("#canvas").hide();
+        navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+              video.src = window.URL.createObjectURL(stream);
+              localStream = stream;
+          });
+      });
+
+  });
+
+  $("#submitpicture").on("click", function(res, callback){
+    $.ajax({
+      url: "/api/get/img",
+      method: "GET"
+    }).then(function(answer){
+      console.log("**********************");
+      console.log(answer);
     });
-}
+  });
 
-document.getElementById("snap").addEventListener("click", function() {
-  
-  $("#canvas").show();
-  context.drawImage(video, 0, 0, 380, 250);
-  $("#video").hide();
-  $("#snap").hide();
-  localStream.getVideoTracks()[0].stop();
+});
 
-  
-});
-});
-});
+//App folder name: socialtvt
+//App Key:  3f7p32exzuxysf4
+//App Secret: c6stmyg6aydj64g
+//token: EtE_0DzV-AcAAAAAAAADHAZ8rRB0huzoKTaJPx8VnxHVQmYH7yzVvVr989u9DFFk
