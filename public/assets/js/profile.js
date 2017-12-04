25cd59b13558a8ee238358b336d387a1a952c4fe
Tabebee <tabibone@gmail.com>
2017-12-03 21:23:06 -0500

    profile being pulled from database. need to style

--- public/assets/js/profile.js
+++ public/assets/js/profile.js
@@ -1,3 +1,21 @@
+var email = localStorage.getItem("email");
+console.log("first: " + email);
+var name;
+var dob;
+var aboutMe; // ????
+
+$.get("/profile/" + email, function (data) {
+    console.log(data);
+    name = data.name;
+    dob = data.dob;
+});
+
+
+
+
+
+
+
 
 
 
