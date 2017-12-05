3829b59e42254b6aaa0c48ce6a73182a1a630577
Tarciso <tarcisoloiola@hotmail.com>
2017-12-04 23:27:49 -0500

    events page

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -173,9 +173,9 @@ module.exports = function(app) {
 
         request('https://app.ticketmaster.com/discovery/v2/events.json?apikey=WfeuZCOCrGxOcUmDfuB6S0QApHBNvGKJ&city=atlanta&classificationName=' + thatone, function (error, response, body) {
             console.log('error:', error); // Print the error if one occurred
-            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-            console.log('body:', body); // Print the HTML for the Google homepage.
-            res.json(body);
+            console.log('statusCode:', response.statusCode); // Print the response status code if a response was received
+            var newBody = JSON.parse(body);
+            res.json(newBody);
         });
         });
     }); //ticketmaster close
