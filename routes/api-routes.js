d549defc5c7a10a6b5b124f674a1c34780e05f9d
Tarciso <tarcisoloiola@hotmail.com>
2018-04-15 19:10:39 -0400

    Update api-routes.js

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,30 +6,16 @@ var request = require('request');
 // Dependencies
 // =============================================================
 
-// Requiring our Burger model
-var Dropbox = require("dropbox");
+// Requiring our model
 var db = require("../models");
 var validator = require("validator");
 // Routes
 // =============================================================
 module.exports = function(app) {
 
-    // POST route for saving a new Burger
+    // POST route for saving a new img
     app.post("/api/get/img", function () {
         console.log(window.location.href)
-
-
-        // console.log("REQUIRE ##########################");
-
-        // var Dropbox = require('dropbox');
-        // var dbx = new Dropbox({ accessToken: 'EtE_0DzV-AcAAAAAAAADHAZ8rRB0huzoKTaJPx8VnxHVQmYH7yzVvVr989u9DFFk' });
-        // dbx.filesListFolder({path: ''})
-        //   .then(function(response) {
-        //     console.log(response);
-        //   })
-        //   .catch(function(error) {
-        //     console.log(error);
-        //   });
     });
 
     app.delete("/api/delete/:id", function (req, res) {
@@ -37,7 +23,6 @@ module.exports = function(app) {
 
         db.users.destroy({}).then(function (dbUser) {
             if (result.changedRows == 0) {
-                // If no rows were changed, then the ID must not exist, so 404
                 return res.status(404).end();
             }
             else {
@@ -51,7 +36,6 @@ module.exports = function(app) {
 
         db.users.update({}).then(function (result) {
             if (result.changedRows == 0) {
-                // If no rows were changed, then the ID must not exist, so 404
                 return res.status(404).end();
             }
             else {
@@ -91,14 +75,11 @@ module.exports = function(app) {
     });
 
     app.get("/api/events/:category", function (req, res) {
-        // console.log("#####################################");
-        // console.log(req.params.category);
         db.events.findAll({
             where: {
                 category: req.params.category
             }
         }).then(function (dbEvents) {
-            // console.log(dbEvents);
             res.json(dbEvents);
         });
     });
@@ -115,15 +96,6 @@ module.exports = function(app) {
             //  convert string back to array
             thatone = answer.survey;
             makeArray = thatone.split(",");
-            console.log("Array???", makeArray);
-
-        // request('https://app.ticketmaster.com/discovery/v2/events.json?apikey=WfeuZCOCrGxOcUmDfuB6S0QApHBNvGKJ&city=atlanta&classificationName=' + thatone, function (error, response, body) {
-        //     console.log('error:', error); // Print the error if one occurred
-        //     console.log('statusCode:', response.statusCode); // Print the response status code if a response was received
-        //     newBody = JSON.parse(body);
-        //     res.json(newBody);
-        // }); // request close
-
             var requestMe = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=WfeuZCOCrGxOcUmDfuB6S0QApHBNvGKJ&city=atlanta&classificationName=";
             var allReq=[];
 
@@ -177,13 +149,7 @@ module.exports = function(app) {
                 }
             })
             .then(function (result) {
-                // testing = testing
-
                 res.redirect("/eventsToAttend");
-
-                // console.log("survey log", user.survey);
-                // console.log("email log", req.params.email);
-                // console.log("testing log",  req.params.testing);
             })
     })
 
